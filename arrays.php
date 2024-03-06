30df5a0bf38cf353f9c0b35540a0ab63f69b443a
Juan <siriuslivebr@gmail.com>
2024-03-06 20:04:56 -0300

    primeiro commit

---
+++ arrays.php
@@ -0,0 +1,22 @@
+<?php
+
+$frutasVermelhas = array (
+  "melancia", "cereja", "morango", "maçã"
+);
+
+$frutasVermelhas2 = [
+  "melancia", "cereja", "morango", "maçã"
+];
+
+$frutasCitricas = array (
+  "Laranja", "Limão", "Tangerina"
+);
+
+echo "\n\nFrutas Vermelhas \n";
+print_r ($frutasVermelhas);
+
+echo "\n\nFrutas Vermelhas2 \n";
+print_r ($frutasVermelhas2);
+
+echo "\n\nFrutas Citricas \n";
+print_r ($frutasCitricas);
---
+++ index.php
@@ -0,0 +1,7 @@
+
+    <?php
+
+    echo "<h2>Olá, <br/> mundo!</h2>";
+
+    ?>
+
